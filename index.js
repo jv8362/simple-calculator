faf2de0e5a23b894c0e8ae7a4d08f1f13bba6955
Jai Akash <157906345+jv8362@users.noreply.github.com>
2024-12-05 17:37:50 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+function appendToDisplay(value) {
+    document.getElementById("display").value += value;
+}
+function calculateResult() {
+    const display = document.getElementById("display");
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
+
+
